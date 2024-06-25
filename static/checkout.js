49de3f45d69c23eef6d6f698cb7f55236546302a
Akbar Khan <akbar.khan@enine.school>
2024-06-25 19:53:35 +0530

    Update:- Included semicolon after javascript statement

--- static/checkout.js
+++ static/checkout.js
@@ -2,28 +2,28 @@ const buttons = document.querySelectorAll('#buy-now-button')
 // Create a Checkout Session
 function initialize() {
   const xhr = new XMLHttpRequest();
-  xhr.open("GET", "/make-payment/", false)
+  xhr.open("GET", "/make-payment/", false);
   xhr.send()
-
+;
   if (xhr.status !== 200) {
     throw new Error("Network response was not ok");
   }
 
-  const session = JSON.parse(xhr.responseText)
-  var stripe = Stripe(session.checkout_public_key)
+  const session = JSON.parse(xhr.responseText);
+  var stripe = Stripe(session.checkout_public_key);
 
   const checkout = stripe.redirectToCheckout({
     sessionId: session.checkout_session_id,
   });
 
   if (checkout.error){
-    console.log(checkout.error)
+    console.log(checkout.error);
   }
 }
 
 buttons.forEach(button=>{
   button.addEventListener("click", function(e){
     e.preventDefault();
-    initialize()
+    initialize();
   })
 });
